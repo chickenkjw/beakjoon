241cfe84fe3078f0a2f3603fe9e9f3582445a439
ariel1910 <chickenkjw@gmail.com>
2023-03-25 18:51:40 +0900

    Update 1620.cpp
    설명 작성

--- 1620/1620.cpp
+++ 1620/1620.cpp
@@ -9,12 +9,15 @@ using namespace std;
 
 int main() {
 	int n, m;
+	// map은 key->번호 찾기 위함
 	map<string, int> pokemon;
+	// vector은 이름을 순서대로 저장해서 숫자->key를 찾기 위함
 	vector<string> pokemon_names;
 	string temp;
 
 	cin >> n >> m;
 
+	//포켓몬 이름과 번호를 입력받고 저장
 	for (int i = 0; i < n; i++) {
 		cin >> temp;
 
@@ -26,9 +29,11 @@ int main() {
 		cin >> temp;
 
 		if (temp[0] >= 65 && temp[0] <= 90) {
+			// 문제 입력받은 것의 첫번째가 문자라면 key값으로 놓고 번호 출력
 			cout << pokemon[temp] << '\n';
 		}
 		else {
+			// 입력받은 것이 숫자라면 숫자로 바꾸어 벡터의 번호에서 출력
 			cout << pokemon_names[stoi(temp)-1] << '\n';
 		}
 	}
